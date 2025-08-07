f6b7b652926e62d1cf7baf8833fed41bfdf722a5
Anton Smolyanyy <146996002+antonsmolyanyy@users.noreply.github.com>
2025-08-07 09:49:47 -0700

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,11 +1,15 @@
 from functools import lru_cache
 from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 class Settings(BaseSettings):
-    descope_project_id: str = "P30sXl6L7lZjl5UT2kYRmgXrWNpl"  # Default for deployment
+    descope_project_id: str = os.getenv("DESCOPE_PROJECT_ID")  # Default for deployment
     descope_api_base_url: str = "https://api.descope.com"  # Default for deployment
-    descope_inbound_app_client_id: str = "UDMwc1hsNkw3bFpqbDVVVDJrWVJtZ1hyV05wbDpUUEEzMHN6bllqcVgwaTRrVDdpQnVrV0hiaUV5Q24="  # Set via environment variable
-    descope_inbound_app_client_secret: str = "yyR9H9JpwBloENrsOr1CUkwpI3oggjgidtD8BgOY4sO"  # Set via environment variable
+    descope_inbound_app_client_id: str = os.getenv("DESCOPE_INBOUND_APP_CLIENT_ID")  # Set via environment variable
+    descope_inbound_app_client_secret: str = os.getenv("DESCOPE_INBOUND_APP_CLIENT_SECRET")  # Set via environment variable
 
     class Config:
         env_file = ".env"
